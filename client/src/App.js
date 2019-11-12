2169c8f0ec8e9e7573d476753cdc04d0d329f808
bellaah <gmltjs1996@naver.com>
2019-11-12 13:28:12 +0900

    fix: eslint와 prettier 연동 버그 수정 prettier와 eslint의 rule이 안맞아서 빨간 줄이 없어지지 않는 문제 발생 prettier설정을 제거하고 eslint의 rule을 바꾸며 자동으로 prettier가 설정되도록 수정

--- client/src/App.js
+++ client/src/App.js
@@ -7,15 +7,13 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Edit<code>src/App.js</code>and save to reload.</p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer">
-          Learn React
+          rel="noopener noreferrer"
+        >Learn React
         </a>
       </header>
     </div>
