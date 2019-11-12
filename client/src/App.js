01b99e2de4afa12f14ccd769f39ed08cf7f63e95
bellaah <gmltjs1996@naver.com>
2019-11-12 13:35:04 +0900

    modify: eslint rules 변경 jsx-closing-bracket-location rule 변경함

--- client/src/App.js
+++ client/src/App.js
@@ -12,8 +12,7 @@ function App() {
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
-          rel="noopener noreferrer"
-        >Learn React
+          rel="noopener noreferrer">Learn React
         </a>
       </header>
     </div>
