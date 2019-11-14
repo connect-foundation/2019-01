fffe079718e3b2ae7d5d8750461f48ac5c82e43a
young-do <yd199323@gmail.com>
2019-11-14 16:15:09 +0900

    build(server): eslint + prettier 설정
    클라이언트에서만 아니라, 서버 개발 시에도 eslint + prettier 설정함.
    eslint 규칙을 airbnb 기준으로 하고 추가 규칙을 설정하지 않았음.
    추후 추가될 예정임.

---
+++ server/bin/util.js
@@ -0,0 +1,59 @@
+/* eslint-disable no-console */
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+  const port = parseInt(val, 10);
+
+  if (Number.isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+/**
+ * Event listener for HTTP server "error" event.
+ */
+function onError(error, port) {
+  if (error.syscall !== 'listen') {
+    throw error;
+  }
+
+  const bind = typeof port === 'string'
+    ? `Pipe ${port}`
+    : `Port ${port}`;
+
+  // handle specific listen errors with friendly messages
+  switch (error.code) {
+    case 'EACCES':
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case 'EADDRINUSE':
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+}
+
+/**
+ * Event listener for HTTP server "listening" event.
+ */
+function onListening(server, debug) {
+  const addr = server.address();
+  const bind = typeof addr === 'string'
+    ? `pipe ${addr}`
+    : `port ${addr.port}`;
+  debug(`Listening on ${bind}`);
+}
+
+module.exports = { normalizePort, onError, onListening };
