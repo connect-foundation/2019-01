cc1240b509cb639e520f3c8395855eca2119b91c
young-do <yd199323@gmail.com>
2019-11-14 16:15:09 +0900

    build(server): eslint + prettier 설정
    클라이언트에서만 아니라, 서버 개발 시에도 eslint + prettier 설정함.
    eslint 규칙을 airbnb 기준으로 하고 추가 규칙을 설정하지 않았음.
    추후 추가될 예정임.

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,5 +1,5 @@
-const nodeExternals = require('webpack-node-externals')
-const path = require('path')
+const nodeExternals = require('webpack-node-externals');
+const path = require('path');
 
 module.exports = {
   target: 'node',
@@ -9,6 +9,6 @@ module.exports = {
     path: path.resolve(__dirname, 'bin'),
     filename: 'app.js',
     library: 'app',
-    libraryTarget:'umd'
+    libraryTarget: 'umd',
   },
-}
+};
