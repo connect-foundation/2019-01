c4a89c3ae4b4a9f995bec95f07a6d50625d4ea14
young-do <yd199323@gmail.com>
2019-11-15 15:50:55 +0900

    ci: 개발 서버에 자동 배포 설정
    자동 배포 설정을 위해 travis ci 설정 변경.
    그리고 script 폴더의 두 파일은 실제 개발 서버에 저장함.

---
+++ script/deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+# pre: 
+#  - 이 파일과 docker-compose.yml 파일은 root 폴더에 저장되어있다.
+#  - 서버에는 docker 와 docker-compose 가 설치되어있다.
+
+# 아래는 보안 이슈로 비어있다.
+# 실제 서버에는 아래에 적절한 값을 입력해야한다.
+# export NCP_ACCESS_KEY_ID=
+# export NCP_SECRET_KEY=
+ 
+echo "$NCP_SECRET_KEY" | docker login -u "$NCP_ACCESS_KEY_ID" dev-2019-01.kr.ncr.ntruss.com --password-stdin
+docker pull dev-2019-01.kr.ncr.ntruss.com/front-end
+docker pull dev-2019-01.kr.ncr.ntruss.com/back-end
+docker-compose down
+docker-compose up
