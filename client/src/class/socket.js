c05b0864d822c9e124692027ebfcc3cd5291b0e2
gyustar <gkagudrbbb@gmail.com>
2019-11-19 22:36:47 +0900

    feat: client socket 연결 구현
    서버와의 통신을 위한 socket class에서 socket 연결 기능 추가

---
+++ client/src/class/socket.js
@@ -0,0 +1,26 @@
+/* eslint-disable no-underscore-dangle */
+import socketio from 'socket.io-client';
+import EVENT from '../constants/socket-event';
+
+class SocketContainer {
+  constructor() {
+    this.socket = undefined;
+    this.connect();
+  }
+
+  connect() {
+    this.socket = socketio.connect('http://localhost:3000');
+  }
+
+  onEnterRoom(callback) {
+    this.socket.on(EVENT.ENTER_ROOM, (data) => callback(data));
+  }
+
+  onEnterPlayer(callback) {
+    this.socket.on(EVENT.ENTER_PLAYER, (data) => callback(data));
+  }
+}
+
+const socket = new SocketContainer();
+
+export default socket;
