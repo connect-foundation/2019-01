14ef0ba9d3f1bfdfc554762ee43926cffcdd6a63
bellaah <gmltjs1996@naver.com>
2019-11-19 15:18:40 +0900

    chore: test를 위해 app.js에서 메서드 실행

--- server/models/quiz.js
+++ server/models/quiz.js
@@ -3,11 +3,12 @@ import quiz from '../database/quiz';
 
 class Quiz {
   /**
-    * @returns {Array.<id,category,level,question,comment,answer>} :quiz object 10개를 return함
+    * @returns {Array.<id,category,level,question,comment,answer>}:quiz object 10개를 return함
     */
   async getTenQuiz() {
     const [rows] = await pool.query(quiz.getTenQuiz);
     const QuizList = rows;
+    console.log(QuizList);
     return QuizList;
   }
 }
