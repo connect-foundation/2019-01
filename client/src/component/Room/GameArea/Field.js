2f9c5bb3c8e2c3c1c5aa1b351acb86defde7aa0a
young-do <yd199323@gmail.com>
2019-11-19 11:30:49 +0900

    feat(client): canvas에서 캐릭터 이미지 이동 구현
    캐릭터 이미지를 담당하는 Class Character 구현 후 React에서 키보드 이벤트로 이동할 수 있도록 구현함. 이미지, 타일 사이즈를 담당하는 것은 상수화하여 파일에 저장함.
    현재 이용하지 않은 socket.io 부분 코드는 주석처리함.

--- client/src/component/Room/GameArea/Field.js
+++ client/src/component/Room/GameArea/Field.js
@@ -1,19 +1,48 @@
-import React from 'react';
-import styled from 'styled-components';
+import React, { useEffect } from 'react';
+import CONSTANTS from './CONSTANTS';
+import Character from './character';
 
-const Canvas = styled.canvas`
-    width: 800px;
-    height: 480px;
-    // box-sizing: border-box;
-    // border: 4px solid grey;
-    background-image: url("https://kr.object.ncloudstorage.com/connect-2019-01/image/field.png");
-`;
+const {
+  CHARACTER, FIELD, TILE, KEYCODE,
+} = CONSTANTS;
+const NICK = 'https://kr.object.ncloudstorage.com/connect-2019-01/image/character/nickfury.png';
+
+const keydownEventHandler = (event, character) => {
+  switch (event.keyCode) {
+    case KEYCODE.LEFT:
+      character.move(CHARACTER.DIRECTION.LEFT);
+      break;
+    case KEYCODE.UP:
+      character.move(CHARACTER.DIRECTION.UP);
+      break;
+    case KEYCODE.RIGHT:
+      character.move(CHARACTER.DIRECTION.RIGHT);
+      break;
+    case KEYCODE.DOWN:
+      character.move(CHARACTER.DIRECTION.DOWN);
+      break;
+    default:
+  }
+};
 
 const Field = () => {
   const canvasRef = React.useRef();
 
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext('2d');
+
+    const myCharacter = new Character(ctx, NICK, 0, 0);
+
+    window.addEventListener('keydown', (event) => keydownEventHandler(event, myCharacter));
+  }, []);
+
   return (
-    <Canvas ref={canvasRef} />
+    <canvas
+      ref={canvasRef}
+      style={{ backgroundImage: `url('${FIELD.BACKGROUND}')` }}
+      width={TILE.WIDTH * FIELD.COLUMN}
+      height={TILE.HEIGHT * FIELD.ROW} />
   );
 };
 
---
+++ client/src/component/Room/GameArea/character.js
@@ -0,0 +1,91 @@
+/* eslint-disable no-underscore-dangle */
+import CONSTANTS from './CONSTANTS';
+
+const { CHARACTER, TILE } = CONSTANTS;
+
+class Character {
+  constructor(ctx, imgUrl, indexX, indexY) {
+    this.ctx = ctx;
+    this.img = new Image();
+    this.img.src = imgUrl;
+    this.indexX = indexX;
+    this.indexY = indexY;
+    this.shape = CHARACTER.SHAPE.STAND;
+    this.direction = CHARACTER.DIRECTION.DOWN;
+    this.curShapeLoopIdx = 0;
+    this.frameCount = 0;
+    this.requestId = undefined;
+
+    this.img.onload = () => this._draw();
+  }
+
+  move(direction) {
+    if (this.requestId) return;
+    this.direction = direction;
+    this.requestId = window.requestAnimationFrame(() => this._walk());
+  }
+
+  _draw() {
+    this.ctx.drawImage(
+      this.img,
+      CHARACTER.SIZE * this.shape + CHARACTER.CROP_OFFSET,
+      CHARACTER.SIZE * this.direction,
+      CHARACTER.WIDTH,
+      CHARACTER.HEIGHT,
+      TILE.WIDTH * this.indexX,
+      TILE.HEIGHT * this.indexY,
+      TILE.WIDTH,
+      TILE.HEIGHT,
+    );
+  }
+
+  _walk() {
+    this.frameCount += 1;
+    if (this.frameCount < CHARACTER.MOVE_FRAME) {
+      this.requestId = window.requestAnimationFrame(() => this._walk());
+      return;
+    }
+    this.frameCount = 0;
+    this._clear();
+    this._step();
+    this._draw();
+    if (this.curShapeLoopIdx >= CHARACTER.SHAPE.LOOP.length) {
+      this.curShapeLoopIdx = 0;
+      this.requestId = undefined;
+    } else {
+      this.requestId = window.requestAnimationFrame(() => this._walk());
+    }
+  }
+
+  _step() {
+    switch (this.direction) {
+      case CHARACTER.DIRECTION.UP:
+        this.indexY -= 1 / CHARACTER.SHAPE.LOOP.length;
+        break;
+      case CHARACTER.DIRECTION.DOWN:
+        this.indexY += 1 / CHARACTER.SHAPE.LOOP.length;
+        break;
+      case CHARACTER.DIRECTION.LEFT:
+        this.indexX -= 1 / CHARACTER.SHAPE.LOOP.length;
+        break;
+      case CHARACTER.DIRECTION.RIGHT:
+        this.indexX += 1 / CHARACTER.SHAPE.LOOP.length;
+        break;
+      default: break;
+    }
+
+    this.shape = CHARACTER.SHAPE.LOOP[this.curShapeLoopIdx];
+    this.curShapeLoopIdx += 1;
+  }
+
+  _clear() {
+    this.ctx.clearRect(
+      TILE.WIDTH * this.indexX,
+      TILE.HEIGHT * this.indexY,
+      TILE.WIDTH,
+      TILE.HEIGHT,
+    );
+  }
+}
+
+export default Character;
