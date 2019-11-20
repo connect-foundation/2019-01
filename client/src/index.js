743abb8f430173626f1cacd80499fe1b4e3c7173
bellaah <gmltjs1996@naver.com>
2019-11-20 11:08:03 +0900

    remove: create-react-app으로 생긴 안쓰는 파일 삭제 logo, css 파일 등 안쓰는 파일등 삭제함

--- client/src/index.js
+++ client/src/index.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import socketio from 'socket.io-client';
-import './index.css';
+
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 /* eslint-disable */
 ReactDOM.render( < App / > , document.getElementById('root'));
 
-// const socket = socketio.connect('http://localhost:3000');
-// (() => {
-//     socket.emit('init', { name: 'bella' });
-
-//     socket.on('welcome', (msg) => {
-//       console.log(msg);
-//     });
-// })();
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
