5f04e03a20337ecc7e9ba2bff210584f152abaa3
young-do <yd199323@gmail.com>
2019-11-21 11:00:02 +0900

    feat(client): 게임 진행 상태, 방장 여부에 따라 전광판 다르게 보이도록 구현
    게임 진행 중에는 문제가 보이고 시간이 카운트 되도록 했으나, 게임 진행 전에는 방장 여부에 따라 게임 스타트 버튼이 보이거나 'waiting' 글씨가 보이도록 구현함.
    서버와의 통신을 가정하고 구현하였기에 연결 후 문제 없는지 확인해 볼 필요 있음.
    
    또한, 하위 컴퍼넌트 생성 시 상위 컴퍼넌트의 배경 이미지가 다시 불러와지는 이슈가 있어, 각 DOM에 style로 적용되도록 함.

--- client/src/component/Room/GameArea/DashBoard.js
+++ client/src/component/Room/GameArea/DashBoard.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from 'react';
-import { DashBoardWrapper, QuizWrapper, CounterWrapper } from './style';
+import {
+  DashBoardWrapper, QuizWrapper, CounterWrapper, GameStartButton, WaitingText,
+} from './style';
+import { DASHBOARD } from '../../../constants/room';
 
 const changeNumberToTwoDigitString = (num) => `${num}`.padStart(2, '0');
 const colorArray = ['red', 'red', 'orange', 'orange', 'green', 'green', 'blue'];
@@ -8,6 +11,15 @@ const getCounterColor = (counter) => (counter >= colorArray.length ? 'black' : c
 const DashBoard = () => {
   const [quiz, setQuiz] = useState('Javascript는 런타임 언어이다');
   const [counter, setCounter] = useState();
+  const [time, setTime] = useState();
+  const [GameStarted, setGameStarted] = useState(false);
+  const [owner, setOwner] = useState(true);
+
+  useEffect(() => {
+    setQuiz('Javascript는 런타임 언어이다');
+    setCounter('--');
+    setTime(10);
+  }, []);
 
   const counterHandler = () => {
     setCounter((_counter) => {
@@ -22,22 +34,37 @@ const DashBoard = () => {
     });
   };
 
-  const startCounter = (initCount) => {
-    setCounter(initCount);
+  const startCounter = () => {
+    setCounter(time);
     setTimeout(counterHandler, 1000);
   };
 
-  useEffect(() => {
-    setQuiz('Javascript는 런타임 언어이다');
+  const startGame = () => {
+    setGameStarted(true);
+    startCounter();
+  };
+
+  const endGame = () => {
+    setGameStarted(false);
     setCounter('--');
-  }, []);
+  };
+
+  const Greeting = () => (
+    owner
+      ? <GameStartButton onClick={startGame}>start( );</GameStartButton>
+      : <WaitingText>waiting...</WaitingText>
+  );
+
+  const QuizOrGreeting = () => (
+    GameStarted
+      ? <QuizWrapper onClick={endGame}>{quiz}</QuizWrapper>
+      : <Greeting />
+  );
 
   // TODO: 카운트 시작하는 방법이 전광판 클릭하는 것. 추후 서버 통신에 의해 시작되도록 변경해야함.
   return (
-    <DashBoardWrapper>
-      <QuizWrapper onClick={() => startCounter(10)}>
-        <div>{quiz}</div>
-      </QuizWrapper>
+    <DashBoardWrapper style={{ backgroundImage: `url("${DASHBOARD.BACKGROUND}")` }}>
+      <QuizOrGreeting />
       <CounterWrapper style={{ color: getCounterColor(counter) }}>
         {changeNumberToTwoDigitString(counter)}
       </CounterWrapper>
