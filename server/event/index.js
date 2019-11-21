a5b38be4a20e65789ada95586d1babebf5262273
gyustar <gkagudrbbb@gmail.com>
2019-11-21 21:54:06 +0900

    review: 리뷰

---
+++ server/event/index.js
@@ -0,0 +1,7 @@
+export default {
+  CONNECTION: 'connection',
+  ROOM_INFOS: 'room_infos',
+  CREATE_ROOM: 'create_room',
+  ENTER_ROOM: 'enter_room',
+  ENTER_ROOM_NEW_PLAYER: 'enter_room_new_player',
+};
---
+++ server/models/room.js
@@ -0,0 +1,66 @@
+class Room {
+  constructor(name, id, player) {
+    this.name = name;
+    this.id = id;
+    this.isEnterable = true;
+    this.quizs = [];
+    this.currentRound = 0;
+    this.currentQuiz = {};
+    this.fieldRow = 8;
+    this.fieldColumn = 16;
+    this.playerList = [player];
+    this.master = player;
+    this.indexOfPlayers = Array(this.fieldColumn).fill(null).map(() => Array(this.fieldRow));
+  }
+
+  /**
+   * @returns {Object{quiz, charactersLocation:Array.<indexX, indexY>, countTime}}
+   */
+  async startNewRound() {
+    this.setToCurrentQuiz();
+    this.currentRound += 1;
+
+    return { quiz: this.currentQuiz, charactersLocation: [], countTime: 60 };
+  }
+
+  addQuizList(quizs) {
+    this.quizs = [...this.quizs, ...quizs];
+  }
+
+  setToCurrentQuiz() {
+    this.currentQuiz = this.quizs[this.currentRound];
+  }
+
+  hasNoMoreQuiz() {
+    return this.quizs[this.currentRound] === undefined;
+  }
+
+  async enterNewPlayer(player) {
+    const character = await this.assignCharacterToPlayer(player);
+    return character;
+  }
+
+  async assignCharacterToPlayer(player) {
+    const [indexX, indexY] = this.getRandomIndex();
+    const character = await player.makeCharacter(indexX, indexY);
+    this.indexOfPlayers[indexX][indexY] = player;
+    this.playerList.push(player);
+    return character;
+  }
+
+  getExistCharacters() {
+    return this.playerList.map((p) => p.character);
+  }
+
+  getRandomIndex() {
+    let indexX;
+    let indexY;
+    do {
+      indexX = Math.floor(Math.random() * (this.fieldColumn));
+      indexY = Math.floor(Math.random() * (this.fieldRow));
+    } while (this.indexOfPlayers[indexX][indexY]);
+    return [indexX, indexY];
+  }
+}
+
+export default Room;
