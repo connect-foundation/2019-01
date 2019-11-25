1ef80567728d2cda00dbefd45135cd33d4137355
bellaah <gmltjs1996@naver.com>
2019-11-25 16:57:37 +0900

    refactor: 코드리뷰받은 사항을 바탕으로 리팩토링 get -> fetch로 바꾸는 등 네이밍 변경함

--- client/src/constants/socket-event.js
+++ client/src/constants/socket-event.js
@@ -2,7 +2,7 @@ const EVENT = {
   ENTER_ROOM: 'enter_room',
   ENTER_PLAYER: 'enter_new_player',
   START_GAME: 'start_game',
-  FETCH_QUIZLIST: 'fetch_quiz_list',
+  FETCH_QUIZLIST: 'get_quiz_list',
 };
 
 export default EVENT;
