e67c122468909df0b83a6f39072434cb88159e82
ktseo41 <ktseo41@gmail.com>
2019-11-25 19:59:30 +0900

    docs: jsdoc 예시 주석 추가(#77)
    - server/models/player.js와 room.js 에 jsdoc 예시 주석 추가
    - 예시로. 참고하기 위함

--- server/models/player.js
+++ server/models/player.js
@@ -1,12 +1,21 @@
 import imageFinder from '../database/image';
-
+/**
+ * A Player Class
+ * @property {Character} Character
+ */
 class Player {
   constructor() {
     this.character = undefined;
   }
 
+  /**
+   *
+   * @param {number} indexX
+   * @param {number} indexY
+   * @returns {Character}
+   */
   async makeCharacter(indexX, indexY) {
-    [this.character] = await imageFinder.fetchRandomCharacter();
+    [this.character] = await imageFinder.getRandomCharacter();
     [this.character.indexX, this.character.indexY] = [indexX, indexY];
     return this.character;
   }
