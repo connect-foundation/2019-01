c4e99c8eb25beff808134bb8a347b6fe7c3b2f18
gyustar <gkagudrbbb@gmail.com>
2019-11-26 17:46:33 +0900

    style: lobby 모델의 필드명 변경
    Map에 어울리게 userList와 roomList의 이름을 복수형으로 변경

--- server/models/lobby.js
+++ server/models/lobby.js
@@ -2,21 +2,21 @@
 
 class Lobby {
   constructor() {
-    this.userList = new Map();
-    this.roomList = new Map();
+    this.users = new Map();
+    this.rooms = new Map();
   }
 
   getRoom(roomId) {
-    return this.roomList.get(roomId);
+    return this.rooms.get(roomId);
   }
 
   _broadcast(emitCallback) {
-    this.userList.forEach(emitCallback);
+    this.users.forEach(emitCallback);
   }
 
   _makeRoomInfos() {
     const roomInfos = [];
-    this.roomList.forEach((room, roomId) => {
+    this.rooms.forEach((room, roomId) => {
       const roomInfo = {
         id: roomId,
         name: room.getName(),
@@ -29,21 +29,22 @@ class Lobby {
   }
 
   enterUser(user) {
-    this.userList.set(user.getId(), user);
+    this.users.set(user.getId(), user);
     user.onEnterLobby();
     user.emitRoomInfos(this._makeRoomInfos());
   }
 
   leaveUser(userId) {
-    this.userList.delete(userId);
+    this.users.delete(userId);
   }
 
   async createRoom(user, room) {
-    this.roomList.set(room.getId(), room);
+    this.rooms.set(room.getId(), room);
     await room.enterUser(user);
     this.leaveUser(user.getId());
   }
 }
 
 const lobby = new Lobby();
+
 export default lobby;
