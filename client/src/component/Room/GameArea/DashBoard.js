53a7f9a87632170757f0b619201a6a484fd77a73
young-do <yd199323@gmail.com>
2019-11-28 18:59:23 +0900

    feat(client): 방장 여부에 따라 전광판 다르게 보이는 기능 구현
    방장 여부에 따라 전광판 다르게 보여서, 방장만 시작 가능함.
    또한 방장이 나간 경우, 새로운 방장인 유저에게 시작 가능하도록 구현함.

--- client/src/component/Room/GameArea/DashBoard.js
+++ client/src/component/Room/GameArea/DashBoard.js
@@ -6,6 +6,7 @@ import {
 import { DASHBOARD } from '../../../constants/room';
 import socket from '../../../class/socket';
 
+const ONE_SECOND = 1000;
 const changeNumberToTwoDigitString = (num) => num.toString().padStart(2, '0');
 const colorArray = ['red', 'red', 'orange', 'orange', 'green', 'green', 'blue'];
 const getCounterColor = (counter) => (counter >= colorArray.length ? 'black' : colorArray[counter]);
@@ -13,14 +14,13 @@ const getCounterColor = (counter) => (counter >= colorArray.length ? 'black' : c
 const DashBoard = () => {
   const [notice, setNotice] = useState('');
   const [counter, setCounter] = useState('--');
-  const [time, setTime] = useState();
   const [GameStarted, setGameStarted] = useState(false);
-  const [owner, setOwner] = useState(true);
+  const [owner, setOwner] = useState(false);
 
   const counterHandler = () => {
     setCounter((_counter) => {
       if (_counter > 1) {
-        setTimeout(counterHandler, 1000);
+        setTimeout(counterHandler, ONE_SECOND);
         return _counter - DASHBOARD.A_SECOND;
       }
       // 여기서 카운트 끝났을 떄 로직 작성하면 됨.
@@ -29,7 +29,7 @@ const DashBoard = () => {
   };
 
   const startCounter = () => {
-    setTimeout(counterHandler, 1000);
+    setTimeout(counterHandler, ONE_SECOND);
   };
 
   const startGame = () => {
@@ -44,16 +44,13 @@ const DashBoard = () => {
    * @param {Array.<Array.<undefined|User>>} roundInfo.characterLocations
    * @param {number} roundInfo.timeLimit
    */
-  const startRound = (roundInfo) => {
-    const {
-      round, question, timeLimit,
-    } = roundInfo;
-
-    if (round === 0) { setGameStarted(true); }
-
-    setCounter(timeLimit);
-    setNotice(question);
-    startCounter();
+  const startRound = ({ round, question, timeLimit }) => {
+    if (round !== undefined) setGameStarted(true);
+    if (question !== undefined) setNotice(question);
+    if (timeLimit !== undefined) {
+      setCounter(timeLimit);
+      startCounter();
+    }
   };
 
   const endRound = ({ round, comment, answer }) => {
@@ -65,6 +62,22 @@ const DashBoard = () => {
   const endGame = () => {
     setGameStarted(false);
     setCounter('--');
+  };
+
+  const enterRoom = ({
+    question, isGameStarted, timeLimit, isOwner,
+  }) => {
+    if (question !== undefined) setNotice(question);
+    if (isGameStarted !== undefined) setGameStarted(isGameStarted);
+    if (isOwner !== undefined) setOwner(isOwner);
+    if (isGameStarted && timeLimit > 0) {
+      setCounter(timeLimit);
+      startCounter();
+    }
+  };
+
+  const leaveUser = ({ isOwner }) => {
+    if (isOwner !== undefined) setOwner(isOwner);
   };
 
   const Greeting = () => (
@@ -81,7 +94,8 @@ const DashBoard = () => {
 
 
   useEffect(() => {
-    setCounter('--');
+    socket.onEnterRoom(enterRoom);
+    socket.onLeaveUser(leaveUser);
     socket.onStartRound(startRound);
     socket.onEndRound(endRound);
     socket.onEndGame(endGame);
