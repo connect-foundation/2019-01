6b01d74aa1dab16166d83dce23f6d211a7dd1114
ktseo41 <ktseo41@gmail.com>
2019-11-29 11:51:05 +0900

    review

--- server/database/image.js
+++
@@ -1,5 +0,0 @@
-import getDataFromDB from './util';
-/* eslint-disable */
-export default {
-    getRandomCharacter: async() => await getDataFromDB(`SELECT * FROM Image WHERE category='character' ORDER BY RAND() LIMIT 1;`)
-};
--- server/models/room.js
+++ server/models/room.js
@@ -1,40 +1,32 @@
-class RoomModel {
-    constructor() {
-      this.isGameStarted = false;
-      this.playerList = []; // socket
-      this.chatLog = [];
-      this.quizs = [];
-      this.currentRound = 0;
-      this.currentQuiz;
-    }
-  
-  
-    async startGame() {
-      this.isGameStarted = true;
-    }
-  
-    /**
-     * @returns {Object{quiz, charactersLocation:Array.<indexX, indexY>, countTime}}
-     */
-    async startNewRound() {
-      this.setToCurrentQuiz();
-      this.currentRound += 1;
-  
-      return { quiz: this.currentQuiz, charactersLocation: [], countTime: 60 };
-    }
-  
-    addQuizList(quizs) {
-      this.quizs = [...this.quizs, ...quizs];
-    }
-  
-    setToCurrentQuiz() {
-      this.currentQuiz = this.quizs[this.currentRound];
-    }
-  
-    hasNoMoreQuiz() {
-      return this.quizs[this.currentRound] === undefined;
-    }
+/**
+ * 코드리뷰
+ * 현재 JSdoc을 사용해주려고 합니다. JSDoc을 사용하면 좋은 것 같은데, 코드가 너무 길어지고 가독성이 좀 떨어지는 느낌이 있는 것 같습니다.
+ * JSDoc과 같이 양이 많은 주석도 많이 사용하나요?
+ */
+
+
+/**
+ * Room Class
+ * @property {string} id
+ * @property {string} name
+ * @property {boolean} isGameStarted
+ * @property {object[]} quizList
+ * @property {object} currentQuiz
+ * @property {number} currentRound
+ * @property {number} currentTime
+ * @property {Map<string, user>} users
+ * @property {Array.<Array.<number, number>>} indexOfCharacters
+ */
+class Room {
+  constructor(id, name) {
+    this.id = id;
+    this.name = name;
+    this.isGameStarted = false;
+    this.quizList = [];
+    this.currentQuiz = {};
+    this.currentRound = 0;
+    this.currentTime = 0;
+    this.users = new Map();
+    this.indexOfCharacters = this._getEmptyIndexMatrix();
   }
-  
-  export default RoomModel;
-  
+}
