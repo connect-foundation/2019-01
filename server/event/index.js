eea66b00345832f3ba59964ab97d32d518dc5376
gyustar <gkagudrbbb@gmail.com>
2019-11-29 10:53:50 +0900

    review: 리뷰 수정

--- server/event/index.js
+++
@@ -1,7 +0,0 @@
-export default {
-  CONNECTION: 'connection',
-  ROOM_INFOS: 'room_infos',
-  CREATE_ROOM: 'create_room',
-  ENTER_ROOM: 'enter_room',
-  ENTER_ROOM_NEW_PLAYER: 'enter_room_new_player',
-};
