7146f6d97f666d10e1806f74b6edcbb7bc8ae38c
bellaah <gmltjs1996@naver.com>
2019-12-03 17:42:36 +0900

    feat: oauth를 통해 데이터를 가져와서 user table에 저장함 oauth로 사용자 데이터를 받아와서 이미 등록된 유저인지 확인한다. 등록되지 않은 유저이면 db table에 추가한다.

--- server/database/image.js
+++ server/database/image.js
@@ -1,4 +1,4 @@
-import query from './util';
+import { query } from './util';
 /* eslint-disable */
 export default {
     fetchRandomCharacter: async() => await query(`SELECT url FROM Image WHERE category='character' ORDER BY RAND() LIMIT 1;`)
