98f620bd312d815795a9ae347149ca725c2f0cff
gyustar <gkagudrbbb@gmail.com>
2019-12-05 13:26:21 +0900

    feat: player 및 viewer 수 실시간 표시 기능 구현
    방 및 게임 정보 제공을 위해 실시간 숫자 표시 기능 구현

--- client/src/components/Room/ChatArea/Info.js
+++ client/src/components/Room/ChatArea/Info.js
@@ -1,13 +1,59 @@
-import React, { useState } from 'react';
-import { CHAT_AREA } from '../../../constants/room';
+import React, { useState, useEffect } from 'react';
+import { ROOM, CHAT_AREA } from '../../../constants/room';
 import {
   ChatHeader, RoomInfo, ExitButton, PlayerInfo, Emoji,
 } from './style';
+import socket from '../../../modules/socket';
 
 const Info = () => {
   const [isGameStarted, setGameStarted] = useState(false);
   const [numOfPlayer, setNumOfPlayer] = useState(0);
   const [numOfViewer, setNumOfViewer] = useState(0);
+
+  const initPlayer = ({ characterList }) => setNumOfPlayer(characterList.length);
+
+  const addPlayer = () => setNumOfPlayer((prevNumOfPlayer) => prevNumOfPlayer + 1);
+
+  const subUser = ({ characterList }) => {
+    const { isAlive } = characterList[0];
+    if (isAlive) {
+      setNumOfPlayer((prevNumOfPlayer) => prevNumOfPlayer - 1);
+      return;
+    }
+    setNumOfViewer((prevNumOfViewer) => prevNumOfViewer - 1);
+  };
+
+  const updateDropUser = ({ characterList }) => {
+    const numOfDropUsers = characterList.length;
+    setNumOfPlayer((prevNumOfPlayer) => prevNumOfPlayer - numOfDropUsers);
+    setNumOfViewer((prevNumOfViewer) => prevNumOfViewer + numOfDropUsers);
+  };
+
+  const updateAliveUser = ({ characterList }) => {
+    const numOfAliveUsers = characterList.length;
+    setNumOfPlayer((prevNumOfPlayer) => {
+      if (prevNumOfPlayer !== numOfAliveUsers) {
+        setNumOfViewer((prevNumOfViewer) => prevNumOfViewer - numOfAliveUsers);
+      }
+      return numOfAliveUsers;
+    });
+  };
+
+  const updateUser = ({ characterList }) => {
+    setTimeout(() => {
+      setNumOfPlayer(characterList.length);
+      setNumOfViewer(0);
+    }, ROOM.WAITING_TIME_MS);
+  };
+
+  useEffect(() => {
+    socket.onEnterRoom(initPlayer);
+    socket.onEnterNewUser(addPlayer);
+    socket.onLeaveUser(subUser);
+    socket.onEndRound(updateDropUser);
+    socket.onStartRound(updateAliveUser);
+    socket.onEndGame(updateUser);
+  }, []);
 
   return (
     <ChatHeader>
--- server/models/room.js
+++ server/models/room.js
@@ -151,11 +151,14 @@ class Room {
     this.nicknameList.push(user.getNickname());
 
     this.users.delete(user.getId());
+    const nickname = user.getNickname();
+    const isAlive = this.aliveUsers.get(nickname) !== undefined;
+    const characterList = [{ nickname, isAlive }];
+
     this.users.forEach((_user) => {
-      const characterList = [{ nickname: user.getNickname() }];
       _user.emitLeaveUser({ characterList, isOwner: this._isOwner(_user) });
     });
-    this.aliveUsers.delete(user.getNickname());
+    this.aliveUsers.delete(nickname);
   }
 
   // emit: start_game / 모든 유저 / (시작 가능 시) 게임 상태 변경
