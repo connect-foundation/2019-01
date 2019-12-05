cca5c2305e6b995fde7e67924655f3ff1698a7db
bellaah <gmltjs1996@naver.com>
2019-12-05 10:37:11 +0900

    modify: 사용하지 않는 url 삭제 api_server_url 삭제함

--- client/src/constants/url.js
+++ client/src/constants/url.js
@@ -2,7 +2,6 @@ const URL = {
   LOCAL_GITHUB_OAUTH: `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_LOCAL_CLIENT_ID}`,
   LOCAL_API_SERVER: 'http://localhost:3000',
   PRODUCTION_GITHUB_OAUTH: `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_PRODUCTION_CLIENT_ID}`,
-  PRODUCTION_API_SERVER: 'http://45.119.146.251/api/',
 };
 
 export default URL;
