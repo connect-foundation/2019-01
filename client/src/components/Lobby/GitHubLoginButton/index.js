de90ea5e3c4994914cd8adb0e6edf3c61ec7deae
ktseo41 <ktseo41@gmail.com>
2019-12-08 15:35:28 +0900

    feat: 로비 github login 기능 구현
    -  github logo 버튼으로 github oauth 인증 및 로그인
    -  github logo 버튼 코드 리팩토링

--- client/src/components/Lobby/GitHubLoginButton/index.js
+++ client/src/components/Lobby/GitHubLoginButton/index.js
@@ -1,26 +1,19 @@
-import React, { useState } from 'react';
-import { LoginButtonWrapper, GitHubIcon, LoginTextWrapper } from './style';
+import React from 'react';
+import {
+  LoginButtonWrapper, GitHubIcon, LoginTextWrapper, GitHubPath,
+} from './style';
+import URL from '../../../constants/url';
 
-const GitHubLoginButton = () => {
-  const [isLogin, setLogin] = useState(false);
-
-  return (
-    <LoginButtonWrapper onClick={() => setLogin(!isLogin)}>
-      {
-        isLogin
-          ? <div>구현중</div>
-          : (
-            <LoginTextWrapper>
-              <GitHubIcon viewBox="0 -0.5 11 11">
-                <metadata>Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj</metadata>
-                <path stroke="#000000" d="M3 0h5M2 1h7M1 2h2M4 2h3M8 2h2M0 3h3M8 3h3M0 4h2M9 4h2M0 5h2M9 5h2M0 6h2M9 6h2M0 7h3M8 7h3M1 8h1M3 8h1M7 8h3M2 9h1M7 9h2M3 10h1M7 10h1" />
-              </GitHubIcon>
-              <div>login</div>
-            </LoginTextWrapper>
-          )
-       }
-    </LoginButtonWrapper>
-  );
-};
-
+const oauthUrl = process.env.NODE_ENV === 'production' ? URL.PRODUCTION_GITHUB_OAUTH : URL.LOCAL_GITHUB_OAUTH;
+const GitHubLoginButton = () => (
+  <LoginButtonWrapper as="a" href={oauthUrl}>
+    <LoginTextWrapper>
+      <GitHubIcon>
+        <metadata>Made with Pixels to Svg https://codepen.io/shshaw/pen/XbxvNj</metadata>
+        <GitHubPath />
+      </GitHubIcon>
+      <div>login</div>
+    </LoginTextWrapper>
+  </LoginButtonWrapper>
+);
 export default GitHubLoginButton;
