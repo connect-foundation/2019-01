7fae81696da2d54129f23aac982f7c19e6eb1f93
ktseo41 <ktseo41@gmail.com>
2019-12-09 17:25:07 +0900

    refactor: 코드 리뷰 반영
    - 필요 없는 async 삭제

--- server/routes/login.js
+++ server/routes/login.js
@@ -5,12 +5,12 @@ import URL from '../constants/url';
 
 const router = express.Router();
 
-router.get('/github', githubOauth, async (req, res) => {
+router.get('/github', githubOauth, (req, res) => {
   const lobbyUrl = process.env.NODE_ENV === 'production' ? URL.PRODUCTION_REACT_LOBBY : URL.LOCAL_REACT_LOBBY;
   res.redirect(lobbyUrl);
 });
 
-router.get('/github/logout', githubLogout, async (req, res) => {
+router.get('/github/logout', githubLogout, (req, res) => {
   res.send(true);
 });
 
