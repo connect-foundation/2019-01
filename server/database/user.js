fd7bbd281690ac573b3a25ac0c2c68d0fdc244af
young-do <yd199323@gmail.com>
2019-12-09 15:40:12 +0900

    fix: 코드리뷰 반영
    User 한명 조회하는 메서드라는 것을 더 명시적으로 할 수 있도록 fetchUser라는 이름으로 변경함.

--- server/database/user.js
+++ server/database/user.js
@@ -4,7 +4,7 @@ export default {
     fetchUserInfo: async(githubId) => await preparedStatement(`SELECT github_id FROM User WHERE github_id = ?;`,githubId),
     registerUser: async(githubId) => await preparedStatement(`INSERT INTO User VALUES (?, false);`,githubId),
     fetchAllUsers: async() => await query(`SELECT * FROM User;`),
-    fetch: async(githubId) => await preparedStatement(`SELECT * FROM User WHERE github_id = ?;`, githubId),
+    fetchUser: async(githubId) => await preparedStatement(`SELECT * FROM User WHERE github_id = ?;`, githubId),
     authorize: async(githubId) => await preparedStatement(`UPDATE User SET is_admin = TRUE WHERE github_id = ?;`, githubId),
     deauthorize: async(githubId) => await preparedStatement(`UPDATE User SET is_admin = FALSE WHERE github_id = ?;`, githubId),
     delete: async(githubId) => await preparedStatement(`DELETE FROM User WHERE github_id = ?;`, githubId),
