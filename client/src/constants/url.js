4de4a303d678e3e8763509afea9bf4aaecdd75b5
ktseo41 <ktseo41@gmail.com>
2019-12-10 11:20:50 +0900

    fix: 배포 환경에 맞게 수정, 게스트 닉네임 반영되지 않던 버그 수정
    -  jwt 쿠키 삭제 후 로비로 리다이렉트

--- client/src/constants/url.js
+++ client/src/constants/url.js
@@ -3,7 +3,7 @@ const URL = {
   LOCAL_API_SERVER: 'http://localhost:3000',
   PRODUCTION_GITHUB_OAUTH: `https://github.com/login/oauth/authorize?client_id=${process.env.REACT_APP_PRODUCTION_CLIENT_ID}`,
   LOCAL_GITHUB_LOGOUT: 'http://localhost:3000/oauth/github/logout',
-  PRODUCTION_GITHUB_LOGOUT: 'http://45.119.146.251:3000/api/oauth/github/logout',
+  PRODUCTION_GITHUB_LOGOUT: 'http://45.119.146.251/api/oauth/github/logout',
 };
 
 export default URL;
