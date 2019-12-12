c74f1f96cff0658ebf30b40e143dd11b874330af
gyustar <gkagudrbbb@gmail.com>
2019-12-12 23:28:48 +0900

    feat: room에서 game start button 효과음, game start 시 효과음 추가
    다이나믹한 게임 요소를 위해 효과음 추가
    sound on/off시 버그를 해결하기 위해 sound on/off 방식을 play/pause가 아닌 muted 로 변경

--- client/src/components/Room/index.js
+++ client/src/components/Room/index.js
@@ -7,29 +7,39 @@ import {
 } from './style';
 import socket from '../../modules/socket';
 import URL from '../../constants/url';
+import { DASHBOARD } from '../../constants/room';
 
 const Room = () => {
   const [backgroundMusic] = useState(new Audio(URL.BACKGROUND_MUSIC));
+  const [buttonClickSound] = useState(new Audio(URL.BUTTON_CLICK_SOUND));
+  const [gameStartSound] = useState(new Audio(URL.GAME_START_SOUND));
   const [musicPlaying, setMusicPlaying] = useState(true);
+
   const { roomId } = useParams();
   const history = useHistory();
 
-  const toggleMusic = () => setMusicPlaying((prevMusicPlaying) => {
-    if (prevMusicPlaying) {
-      backgroundMusic.pause();
-      return false;
-    }
+  const toggleMusic = () => {
+    setMusicPlaying(!musicPlaying);
+    backgroundMusic.muted = !backgroundMusic.muted;
+    buttonClickSound.muted = !buttonClickSound.muted;
+    gameStartSound.muted = !gameStartSound.muted;
+  };
 
-    backgroundMusic.play();
-    return true;
-  });
+  const playSoundEffect = () => {
+    backgroundMusic.pause();
+    gameStartSound.play();
+  };
+
+  const replayBackgroundMusic = () => setTimeout(() => backgroundMusic.play(), DASHBOARD.SECOND_MS);
 
   useEffect(() => {
     backgroundMusic.autoplay = true;
     backgroundMusic.loop = true;
+    gameStartSound.onended = replayBackgroundMusic;
 
     if (socket.isConnected() === false) history.replace('/');
     socket.emitEnterRoom(roomId);
+    socket.onStartGame(playSoundEffect);
     socket.onEndGame(({ isOwner }) => {
       if (isOwner) socket.emitEndGame(roomId);
     });
@@ -45,7 +55,7 @@ const Room = () => {
         <SoundToggle onClick={toggleMusic}>{musicPlaying ? 'sound on' : 'sound off'}</SoundToggle>
       </SoundToggleWrapper>
       <RoomWrapper>
-        <GameArea />
+        <GameArea buttonClickSound={buttonClickSound} />
         <ChatArea />
       </RoomWrapper>
     </Wrapper>
