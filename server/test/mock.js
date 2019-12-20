5cdc41c77325d9e9cf7a1de97cf67d2215c051ad
gyustar <gkagudrbbb@gmail.com>
2019-12-20 11:30:32 +0900

    feat: socket.io 테스트 추가 작성 방 만들고 그 방에 다른 client가 들어가는 evnet발생을 위하여 client 2개 생성 한 client가 방을 만들고 다른 클라이언트가 방이 있는지 확인하는 테스트 코드 작성

--- server/test/mock.js
+++ server/test/mock.js
@@ -1,4 +1,6 @@
 export default {
   ENTER_RESPONSE_KEYS: ['characterList', 'isGameStarted', 'isOwner', 'timeLimit'],
   MOVE_RESPONSE_KEYS: ['canMove', 'nickname', 'direction', 'newIndexX', 'newIndexY'],
+  ROOM_INFO_KEYS: ['id', 'name', 'numOfUsers', 'isEnterable'],
+  KNOCK_ROOM_KEYS: ['isEnterable', 'roomId', 'message'],
 };
--- server/test/socket-server.test.js
+++ server/test/socket-server.test.js
@@ -1,63 +1,92 @@
 import mock from './mock';
 
-const { ENTER_RESPONSE_KEYS, MOVE_RESPONSE_KEYS } = mock;
+const {
+  ENTER_RESPONSE_KEYS, MOVE_RESPONSE_KEYS, ROOM_INFO_KEYS, KNOCK_ROOM_KEYS,
+} = mock;
 
 const ioClient = require('socket.io-client');
 const http = require('http');
 const { app, socketIo } = require('../bin/app').default;
 
-let socket;
+let clientSocket1;
+let clientSocket2;
 let httpServer;
 let httpServerAddr;
 
-describe('socket.io test', () => {
-  beforeAll(() => {
-    httpServer = http.createServer(app).listen('5001');
-    socketIo.attach(httpServer);
-    httpServerAddr = httpServer.address();
+beforeAll(() => {
+  httpServer = http.createServer(app).listen('5001');
+  socketIo.attach(httpServer);
+  httpServerAddr = httpServer.address();
 
-    socket = ioClient.connect(`http://[${httpServerAddr.address}]:${httpServerAddr.port}`, {
-      'reconnection delay': 0,
-      'reopen delay': 0,
-      'force new connection': true,
-      transports: ['websocket'],
-    });
+  const connectUrl = `http://[${httpServerAddr.address}]:${httpServerAddr.port}`;
+  const connectOption = {
+    'reconnection delay': 0,
+    'reopen delay': 0,
+    'force new connection': true,
+    transports: ['websocket'],
+  };
+
+  clientSocket1 = ioClient.connect(connectUrl, connectOption);
+  clientSocket2 = ioClient.connect(connectUrl, connectOption);
+});
+
+afterAll(() => {
+  if (clientSocket1.connected) {
+    clientSocket1.disconnect();
+  }
+
+  if (clientSocket2.connected) {
+    clientSocket2.disconnect();
+  }
+
+  socketIo.close();
+  httpServer.close();
+});
+
+describe('socket.io connect test', () => {
+  test('client1 connect test', (done) => {
+    clientSocket1.on('connect', () => done());
   });
 
-  afterAll(() => {
-    if (socket.connected) {
-      socket.disconnect();
-    }
-    socketIo.close();
-    httpServer.close();
+  test('client2 connect test', (done) => {
+    clientSocket2.on('connect', () => done());
   });
+});
 
-  test('connect test', (done) => {
-    socket.on('connect', () => {
+describe('lobby event test', () => {
+  let roomId;
+
+  test('[EMIT] client1 \'enter_lobby\' event test', (done) => {
+    clientSocket1.once('enter_lobby', (message) => {
+      expect(message).toEqual([]);
       done();
     });
+    clientSocket1.emit('enter_lobby');
   });
 
-  test('[EMIT] \'enter_room\' event test', (done) => {
-    socket.once('enter_room', (message) => {
-      expect(ENTER_RESPONSE_KEYS).toEqual(expect.arrayContaining(Object.keys(message)));
+  test('[EMIT] client1 \'create_room\' event test', (done) => {
+    clientSocket1.once('create_room', (message) => {
+      roomId = message;
+      expect(typeof message).toEqual('string');
       done();
     });
-    socket.emit('enter_room', 1);
+    clientSocket1.emit('create_room', 'room name');
   });
 
-  test('\'move\' event test', (done) => {
-    socket.once('move', (message) => {
-      if (!message.canMove) done();
-
-      expect(MOVE_RESPONSE_KEYS).toEqual(expect.arrayContaining(Object.keys(message)));
+  test('[EMIT] client2 \'enter_lobby\' event test2', (done) => {
+    clientSocket2.once('enter_lobby', (message) => {
+      expect(ROOM_INFO_KEYS).toEqual(expect.arrayContaining(Object.keys(message[0])));
       done();
     });
-    socket.emit('move', 0);
+    clientSocket2.emit('enter_lobby');
   });
 
-  test('[EMIT] \'leave_room\' event test', (done) => {
-    socket.once('leave_room', () => done());
-    socket.emit('leave_room', 1);
+  test('[EMIT] client2 \'knock_room\' event test2', (done) => {
+    clientSocket2.once('knock_room', (message) => {
+      expect(KNOCK_ROOM_KEYS).toEqual(expect.arrayContaining(Object.keys(message)));
+      expect(message[KNOCK_ROOM_KEYS[0]]).toBeTruthy();
+      done();
+    });
+    clientSocket2.emit('knock_room', roomId);
   });
 });
