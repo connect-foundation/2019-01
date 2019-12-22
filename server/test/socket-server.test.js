92117f3bd1604d2dea9a2e7e7ae29dda841e8d7c
gyustar <gkagudrbbb@gmail.com>
2019-12-22 12:34:12 +0900

    feat: socket game event test 코드 작성
    start game, start round event test 코드 작성

--- server/test/socket-server.test.js
+++ server/test/socket-server.test.js
@@ -1,17 +1,21 @@
 import mock from './mock';
+import Client from './client';
 
 const {
-  ENTER_RESPONSE_KEYS, MOVE_RESPONSE_KEYS, ROOM_INFO_KEYS, KNOCK_ROOM_KEYS,
+  ENTER_RESPONSE_KEYS, MOVE_RESPONSE_KEYS, ROOM_INFO_KEYS,
+  KNOCK_ROOM_KEYS, START_ROUND_KEYS,
 } = mock;
 
 const ioClient = require('socket.io-client');
 const http = require('http');
 const { app, socketIo } = require('../bin/app').default;
 
-let clientSocket1;
-let clientSocket2;
 let httpServer;
 let httpServerAddr;
+
+const roomName = 'test room name';
+const ownerClient = new Client();
+const playerClient = new Client();
 
 beforeAll(() => {
   httpServer = http.createServer(app).listen('5001');
@@ -26,17 +30,20 @@ beforeAll(() => {
     transports: ['websocket'],
   };
 
-  clientSocket1 = ioClient.connect(connectUrl, connectOption);
-  clientSocket2 = ioClient.connect(connectUrl, connectOption);
+  const ownerSocket = ioClient.connect(connectUrl, connectOption);
+  const playerSocket = ioClient.connect(connectUrl, connectOption);
+
+  ownerClient.setSocket(ownerSocket);
+  playerClient.setSocket(playerSocket);
 });
 
 afterAll(() => {
-  if (clientSocket1.connected) {
-    clientSocket1.disconnect();
+  if (ownerClient.isConnected()) {
+    ownerClient.disconnect();
   }
 
-  if (clientSocket2.connected) {
-    clientSocket2.disconnect();
+  if (playerClient.isConnected()) {
+    playerClient.disconnect();
   }
 
   socketIo.close();
@@ -44,49 +51,108 @@ afterAll(() => {
 });
 
 describe('socket.io connect test', () => {
-  test('client1 connect test', (done) => {
-    clientSocket1.on('connect', () => done());
+  test('owner client connect test', (done) => {
+    ownerClient.once('connect', () => done());
   });
 
-  test('client2 connect test', (done) => {
-    clientSocket2.on('connect', () => done());
+  test('player client connect test', (done) => {
+    playerClient.once('connect', () => done());
   });
 });
 
 describe('lobby event test', () => {
-  let roomId;
-
-  test('[EMIT] client1 \'enter_lobby\' event test', (done) => {
-    clientSocket1.once('enter_lobby', (message) => {
+  test('[EMIT] owner client \'enter_lobby\' event test', (done) => {
+    ownerClient.once('enter_lobby', (message) => {
       expect(message).toEqual([]);
       done();
     });
-    clientSocket1.emit('enter_lobby');
+    ownerClient.emit('enter_lobby');
   });
 
-  test('[EMIT] client1 \'create_room\' event test', (done) => {
-    clientSocket1.once('create_room', (message) => {
-      roomId = message;
+  test('[EMIT] owner client \'create_room\' event test', (done) => {
+    ownerClient.once('create_room', (message) => {
       expect(typeof message).toEqual('string');
+      ownerClient.setRoomId(message);
       done();
     });
-    clientSocket1.emit('create_room', 'room name');
+    ownerClient.emit('create_room', roomName);
   });
 
-  test('[EMIT] client2 \'enter_lobby\' event test2', (done) => {
-    clientSocket2.once('enter_lobby', (message) => {
+  test('[EMIT] player client \'enter_lobby\' event test2', (done) => {
+    playerClient.once('enter_lobby', (message) => {
       expect(ROOM_INFO_KEYS).toEqual(expect.arrayContaining(Object.keys(message[0])));
       done();
     });
-    clientSocket2.emit('enter_lobby');
+    playerClient.emit('enter_lobby');
   });
 
-  test('[EMIT] client2 \'knock_room\' event test2', (done) => {
-    clientSocket2.once('knock_room', (message) => {
+  test('[EMIT] player client \'knock_room\' event test2', (done) => {
+    playerClient.once('knock_room', (message) => {
       expect(KNOCK_ROOM_KEYS).toEqual(expect.arrayContaining(Object.keys(message)));
       expect(message[KNOCK_ROOM_KEYS[0]]).toBeTruthy();
       done();
     });
-    clientSocket2.emit('knock_room', roomId);
+    const roomId = ownerClient.getRoomId();
+    playerClient.emit('knock_room', roomId);
+  });
+});
+
+describe('room event test', () => {
+  test('[EMIT] owner client \'enter_room\' event test2', (done) => {
+    ownerClient.once('enter_room', (message) => {
+      expect(ENTER_RESPONSE_KEYS).toEqual(expect.arrayContaining(Object.keys(message)));
+      expect(message.isOwner).toBeTruthy();
+      expect(message.roomName).toBe(roomName);
+      done();
+    });
+    const roomId = ownerClient.getRoomId();
+    ownerClient.emit('enter_room', roomId);
+  });
+
+  test('[EMIT] player client \'enter_room\' event test2', (done) => {
+    const roomId = ownerClient.getRoomId();
+    playerClient.once('enter_room', (message) => {
+      expect(ENTER_RESPONSE_KEYS).toEqual(expect.arrayContaining(Object.keys(message)));
+      expect(message.isOwner).toBeFalsy();
+      expect(message.roomName).toBe(roomName);
+      playerClient.setRoomId(roomId);
+      done();
+    });
+    playerClient.emit('enter_room', roomId);
+  });
+});
+
+describe('move event test', () => {
+  test('[EMIT] owner client \'move\' event test', (done) => {
+    ownerClient.once('move', (message) => {
+      expect(MOVE_RESPONSE_KEYS).toEqual(expect.arrayContaining(Object.keys(message)));
+      done();
+    });
+    ownerClient.emit('move', 0);
+  });
+});
+
+describe('game event test', () => {
+  test('[EMIT] owner client \'start game\' event test', (done) => {
+    ownerClient.once('start_game', () => done());
+    ownerClient.emit('start_game');
+  });
+
+  test('[EMIT] player client \'start game\' event test', (done) => {
+    playerClient.once('start_game', () => done());
+  });
+
+  test('[EMIT] owner client \'start round\' event test', (done) => {
+    ownerClient.once('start_round', (message) => {
+      expect(START_ROUND_KEYS).toEqual(expect.arrayContaining(Object.keys(message)));
+      done();
+    });
+  });
+
+  test('[EMIT] player client \'start round\' event test', (done) => {
+    playerClient.once('start_round', (message) => {
+      expect(START_ROUND_KEYS).toEqual(expect.arrayContaining(Object.keys(message)));
+      done();
+    });
   });
 });
